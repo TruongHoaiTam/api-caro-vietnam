f7968baeac3782e88e21ca35b1752f229dda0f5d
LAPTOP-L520LRO6\Asus <truonghoaitam.t13cla@gmail.com>
2019-10-28 21:23:34 +0700

    fix

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.listen(port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header("Access-Control-Allow-Headers", 'Origin, Access-Control-Allow-Methods, X-Requested-With, Content-Type, Accept, Authorization ');
+    next();
+});
+
 app.use('/', require('./app/routes/index'));
 
 app.use((req, res, next) => {
